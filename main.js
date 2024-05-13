27a5a97d63de160d10f015bbc6ba55443ee14432
GaRIEN <gabrielhuaman68364gmail.com>
2024-05-13 15:57:15 -0500

    creacion de la funcion gatosfavoritos, para listar guardar y listar gatos favoritos

--- main.js
+++ main.js
@@ -1,26 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-  async function recargarGatos() {
-    const URL =
-      "https://api.thecatapi.com/v1/images/search?limit=4&api_key=live_onj9ZIxmbQgIReBzzxPqF6sncPCvSYRf1KEqc68m1xeGN6pp3Od5jCzpVlFNK7aW";
-    const res = await fetch(URL);
-    const data = await res.json();
-    console.log("los gatitos son: ", data);
-  
-    const gatito = document.getElementById("gatito");
-    gatito.innerHTML = "";
-  
-    data.forEach((gato) => {
-      gatito.insertAdjacentHTML(
-        'beforeend',
-        `<div id=${gato.id}>
-        <img class="clase-gato" src="${ gato.url}" > </img>
-        <p class="icon-favorito"> <i class="fa-sharp fa-solid fa-heart"></i> </p>
-        </div>`
-      );
-    });
-  }
+
   
   recargarGatos();
+  GatosFavoritos()
+
   const Masgatos = document.getElementById("button-mas-gatod");
   Masgatos.addEventListener("click", function () {
     recargarGatos();
@@ -36,6 +19,49 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
+  //FUNCION PARA TRAER GATOS DE LA API
+  async function recargarGatos() {
+    const URL =
+      "https://api.thecatapi.com/v1/images/search?limit=15&api_key=live_onj9ZIxmbQgIReBzzxPqF6sncPCvSYRf1KEqc68m1xeGN6pp3Od5jCzpVlFNK7aW";
+    const res = await fetch(URL);
+    const data = await res.json();
+    console.log("los gatitos son: ", data);
+  
+    const gatito = document.getElementById("gatito");
+    gatito.innerHTML = "";
+  
+    data.forEach((gato) => {
+      gatito.insertAdjacentHTML(
+        'beforeend',
+        `<div >
+        <img class="clase-gato" src="${ gato.url}" > </img>
+        <p id=${gato.id} class="icon-favorito"> <i class="fa-sharp fa-solid fa-heart"></i> </p>
+        </div>`
+      );
+    });
+  }
+
+  //FUNCION PARA TRAER GATOS FAVORITOS
+  async function GatosFavoritos() {
+    const URL =
+      "https://api.thecatapi.com/v1/favourites?limit=2&api_key=live_onj9ZIxmbQgIReBzzxPqF6sncPCvSYRf1KEqc68m1xeGN6pp3Od5jCzpVlFNK7aW";
+    const res = await fetch(URL);
+    const data = await res.json();
+    console.log("los gatitos favoritos son: ", data);
+  
+    const gatitos_favoritos = document.getElementById("gatitos_favoritos");
+    gatitos_favoritos.innerHTML = "";
+  
+    data.forEach((gato) => {
+      gatitos_favoritos.insertAdjacentHTML(
+        'beforeend',
+        `<div>
+        <img class="clase-gato" src="${ gato.url}" > </img>
+        <p id=${gato.id} class="icon-favorito"> <i class="fa-sharp fa-solid fa-heart"></i> </p>
+        </div>`
+      );
+    });
+  }
 
 
 });
